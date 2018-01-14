fc3029de5ccb7290ab5d95f10f135e95d718c590
NequLW <mackor002@utp.edu.pl>
2018-01-14 10:46:38 +0100

    fixing

---
+++ krab.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int a;
+int b;
+cout<< "Podaj liczbe a: ";
+cin>>a;
+cout<< "Podaj liczbe b: "<<endl;
+cin>>b;
+cout<< "Wynik mnozenia: <<a*b;
+
+
+
+return 0;
+{
